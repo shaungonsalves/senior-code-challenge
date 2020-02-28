df2ae11a28cd8cabe451f5e054223d8cc77417a5
shaun-PC <shaun.gonsalves288@gmail.com>
2020-02-28 12:23:35 -0500

    1st case Kinda working

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
@@ -32,8 +34,23 @@ public class EmployeeController {
             e.setLastName(employeeService.read(sub_id).getLastName());
             e.setPosition(employeeService.read(sub_id).getPosition());
             e.setDepartment(employeeService.read(sub_id).getDepartment());
-            e.setDirectReports(employeeService.read(sub_id).getDirectReports());
-            e.setNumberOfReports(employeeService.read(sub_id).getNumberOfReports());
+            List<Employee> employeesList = employeeService.read(sub_id).getDirectReports();
+            e.setDirectReports(employeesList);
+            if (employeesList!=null) {
+                e.setNumberOfReports(employeesList.size());
+                for (Employee e2 : employeesList) {
+                    String sub_id2 = e2.getEmployeeId();
+                    e2.setFirstName(employeeService.read(sub_id2).getFirstName());
+                    e2.setLastName(employeeService.read(sub_id2).getLastName());
+                    e2.setPosition(employeeService.read(sub_id2).getPosition());
+                    e2.setDepartment(employeeService.read(sub_id2).getDepartment());
+                    List<Employee> employeesList2 = employeeService.read(sub_id2).getDirectReports();
+                    e2.setDirectReports(employeesList2);
+                    if (employeesList2 != null) {
+                        e2.setNumberOfReports(employeesList2.size());
+                    }
+                }
+            }
         }
         return es;
     }
