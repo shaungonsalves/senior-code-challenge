5f808555fe596da662898f2716f4474874ecf3b8
shaun-PC <shaun.gonsalves288@gmail.com>
2020-08-15 19:46:28 -0400

    Task 1 completed

---
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -0,0 +1,23 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportingStructureController {
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/structure/{id}")
+    public ReportingStructure getReportingStructure(@PathVariable String id){
+         return employeeService.readReportingStructure(id);
+    }
+}
--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,12 +2,14 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -46,5 +48,30 @@ public class EmployeeServiceImpl implements EmployeeService {
         LOG.debug("Updating employee [{}]", employee);
 
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public ReportingStructure readReportingStructure(String id) {
+        Employee employee = read(id);
+
+        Integer numberOfReports = getNumberOfReports(employee);
+
+        ReportingStructure reportingStructure = new ReportingStructure();
+        reportingStructure.setEmployee(employee);
+        reportingStructure.setNumberOfReports(numberOfReports);
+
+        return reportingStructure;
+    }
+
+    private Integer getNumberOfReports(Employee employee) {
+        List<Employee> directReports = employee.getDirectReports();
+        if (directReports==null){
+            return 0;
+        }
+        Integer numberOfReports = 0;
+        for(Employee emp:directReports) {
+            numberOfReports = numberOfReports+ employee.getDirectReports().size();
+        }
+        return numberOfReports;
     }
 }
